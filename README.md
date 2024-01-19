31f43931ca5ce22cb9f19d19e03c5cfdbf55e9b3
Donizete Debroski <donizetedebroski@gmail.com>
2024-01-19 09:07:57 -0300

    Primeiro Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vagrant-kubernets
+
+Ambiente para simulação e testes do kubernets utilizando o vagrant e o virtualbox para simular o ambiente.
+
+Estrutura com 2gb de RAM e 2 core para duas cada maquinas.
---
+++ cluster-kubernets/provision-control-node.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+sudo apt update
+
+sudo swapoff -a
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+# Instalando os pacotes do Kubernetes
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+# Instalando o containerd
+# container runtime
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get install -y containerd.io
+
+# Configurando o containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl status containerd
+
+# Habilitando o serviço do kubelet
+sudo systemctl enable --now kubelet
+
